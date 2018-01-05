09e353de54f527088412186bedd707c78e8a1d3c
Tomáš Křen <tomkren@gmail.com>
2018-01-05 18:07:39 +0100

    Simple selection of motors.

--- scripts/laser.py
+++ scripts/laser.py
@@ -6,55 +6,67 @@ import sys
 
 laser = o1 = eh.output.one
 m1 = eh.motor.one
+m2 = eh.motor.two
 
 speed = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 t = float(sys.argv[2]) if len(sys.argv) > 2 else 0.25
+motor_id = int(sys.argv[3]) if len(sys.argv) > 3 else 1
 
-rounds = 5
-steps = 15
-
-def main2():
-
-	laser.on()
-
-	m1.speed(speed)
-	time.sleep(t)
-	m1.stop()
+motor = m1 if motor_id == 1 else m2
 
 
+def main():
 
+        laser.on()
+
+        motor.speed(speed)
+        time.sleep(t)
+        motor.stop()
+
+        time.sleep(1)
+
+
+
+
+        
 def step(dir=1):
-	m1.speed(17*dir)
-	time.sleep(0.05)
-	m1.stop()
-
-
-def main1():
-	for round in range(rounds):
-
-		print("{} / {}".format(round+1, rounds))
-
-		for i in range(steps):
-			o1.on()
-			time.sleep(0.05)
-			o1.off()
-			time.sleep(0.05)
-
-		o1.on()
-
-		for i in range(steps):
-			step()
-			time.sleep(0.02)
-
-		for i in range(steps*2):
-			step(-1)
-			time.sleep(0.02)
-
-		for i in range(steps):
-			step()
-			time.sleep(0.02)
+        m1.speed(17*dir)
+        time.sleep(0.05)
+        m1.stop()
 
 
 
-# main1()
-main2()
+
+def main_old():
+
+        rounds = 5
+        steps = 15
+        
+        for round in range(rounds):
+
+                print("{} / {}".format(round+1, rounds))
+
+                for i in range(steps):
+                        o1.on()
+                        time.sleep(0.05)
+                        o1.off()
+                        time.sleep(0.05)
+
+                o1.on()
+
+                for i in range(steps):
+                        step()
+                        time.sleep(0.02)
+
+                for i in range(steps*2):
+                        step(-1)
+                        time.sleep(0.02)
+
+                for i in range(steps):
+                        step()
+                        time.sleep(0.02)
+
+
+
+# main_old()
+main()
